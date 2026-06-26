145847fd57ca4aae96c2ebb39f6ae3a006d9aadc
yana-347 <yanaodanzig@gmail.com>
2026-06-26 14:33:46 -0400

    Prevent stale survey assets

--- build/dev-server.js
+++ build/dev-server.js
@@ -11,6 +11,14 @@ export function reload(done) {
 export function serve(done) {
     server.init({
         notify: false,
+        middleware: [
+            (req, res, next) => {
+                res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+                res.setHeader("Pragma", "no-cache");
+                res.setHeader("Expires", "0");
+                next();
+            }
+        ],
         server: {
             baseDir: "./docs/",
             serveStaticOptions: {
